8b9b4ca719976beb8d83cb4d548ad4a62cd84fee
hdy <35fwangdo@gmail.com>
2025-09-06 10:30:46 +0900

    feat(Header): Add component to show header

--- src/components/Colors.tsx
+++ src/components/Colors.tsx
@@ -1,3 +1,15 @@
 // for colors. 
 
 export const greyColor = "#888888" 
+
+export const theme = {
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,49 @@
 import { Link, useMatch, useLocation } from 'react-router-dom'; 
 import { useAuthStore } from '../stores/authStore';
-import { greyColor } from './Colors';
+import { greyColor, theme } from './Colors';
+import type { Bold } from 'lucide-react';
+
+interface IItem {
+    color?: string; 
+    children: React.ReactNode; 
+}
+
+function Item({ color = theme.white.lighter, children, ...props}: IItem) {
+    return (
+        <li
+            {...props}
+            className={`text-[${color}]`}
+        >
+            {children}
+        </li>
+    )
+}
+
+function LinkWrapper({children, to, ...props}: {children: React.ReactNode, to: string}) {
+    return (
+        <Link 
+            {...props}
+            to={to}
+            className={`
+                no-underline transition-colors duration-300 ease-in-out hover:[color:${theme.black.lighter}] 
+            `}
+        >
+            {children}
+        </Link>
+    )
+}
+
+function BoldWrapper({children, ...props}: {children: React.ReactNode}) {
+    return (
+        <span
+            {...props}
+            className='font-bold'
+        >
+            {children}
+        </span>
+    )
+}
+
 
 function Header() {
     const homeMatch = useMatch('/'); 
@@ -17,8 +60,14 @@ function Header() {
 
     return (
         // content -> padding -> border -> margin. 
-        <div className={`flex w-full h-[5vh] m-0 p-0`}>
-            {/* TODO */}
+        <div className={`flex w-full h-[5vh] m-0 p-0 justify-between items-center bg-[${greyColor}] px-[15vw]`}>
+            <ul className={`flex gap-x-[30px]`}>
+                <Item><LinkWrapper to="/">{homeMatch ? <BoldWrapper>Home</BoldWrapper> : "Home"}</LinkWrapper></Item>
+                <Item><LinkWrapper to="/Market">{marketMatch ? <BoldWrapper>Market</BoldWrapper> : "Market"}</LinkWrapper></Item>
+                <Item><LinkWrapper to="/Mine">{mineMatch ? <BoldWrapper>Mine</BoldWrapper> : "Mine"}</LinkWrapper></Item>
+                <Item><LinkWrapper to="/Review">{reviewsMatch ? <BoldWrapper>Review</BoldWrapper> : "Review"}</LinkWrapper></Item>
+                <Item><LinkWrapper to="/Community">{comuMatch ? <BoldWrapper>Community</BoldWrapper> : "Community"}</LinkWrapper></Item>
+            </ul>
         </div>
     )
     
